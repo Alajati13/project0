0badd2f036998af4c8ad2bb280f6a92d459146fb
alajati13 <alajati13@hotmail.com>
2022-06-22 00:37:55 +0400

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-names = ["ali", "hiba", "tim"]
+def validate(sum):
+    def wrapper():
+        print("please enter two numbers to get their sum")
+        print(f"The sum is : {sum()}")
+    return wrapper
 
-for i in names:
-    print(i)
+        
+
+
+
+@validate
+def sum():
+    x = int(input("X : "))
+    y = int(input("Y : "))
+    z = x + y
+    return z
+
+sum()
